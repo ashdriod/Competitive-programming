f3309d1b4d4bcaefa4be9946531ea6db6d4aa6f4
ASHWIN VAZHAPPILLY <ashwinvazhappilly@gmail.com>
2020-05-13 01:28:17 +0530

    Create Knapsack.c

---
+++ Knapsack.c
@@ -0,0 +1,35 @@
+
+#include<stdio.h>
+
+int max(int a, int b) { return (a > b)? a : b; }
+
+int knapSack(int W, int wt[], int val[], int n)
+{
+int i, w;
+int K[n+1][W+1];
+
+for (i = 0; i <= n; i++)
+{
+	for (w = 0; w <= W; w++)
+	{
+		if (i==0 || w==0)
+			K[i][w] = 0;
+		else if (wt[i-1] <= w)
+				K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]], K[i-1][w]);
+		else
+				K[i][w] = K[i-1][w];
+	}
+}
+
+return K[n][W];
+}
+
+int main()
+{
+	int val[] = {10,15,40};
+	int wt[] = {1,2,3};
+	int W = 6;
+	int n = sizeof(val)/sizeof(val[0]);
+	printf("%d", knapSack(W, wt, val, n));
+	return 0;
+}
